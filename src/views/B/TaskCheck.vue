036471e82dadb6bc617f490d2d48ed079a63492c
Sail <151263555@qq.com>
2018-05-15 00:36:20 +0800

    B端验收星级评价

--- src/views/B/TaskCheck.vue
+++ src/views/B/TaskCheck.vue
@@ -5,6 +5,99 @@
             <div>
                 <iframe style="width: 960px; height: 642px; margin: 0 auto;display: block;" frameBorder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" v-if="iframeSrc" :src="iframeSrc" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
             </div>
+            <div class="star-rating-wrap">
+              <h3 class="main-title">{{$lang('星级评价')}}</h3>
+              <div class="rating-wrap">
+                <div class="star-rating">
+                  <div class="star-rating-item" @click="starRating(1)" v-bind:class="{'star-rating-selected': star_num === 1}">
+                    <div class="rating-label1">
+                      十分不满意
+                    </div>
+                    <div class="rating-label2">
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                    </div>
+                  </div>
+                  <div class="star-rating-item" @click="starRating(2)" v-bind:class="{'star-rating-selected': star_num === 2}">
+                    <div class="rating-label1">
+                      不满意
+                    </div>
+                    <div class="rating-label2">
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                    </div>
+                  </div>
+                  <div class="star-rating-item" @click="starRating(3)" v-bind:class="{'star-rating-selected': star_num === 3}">
+                    <div class="rating-label1">
+                      合格
+                    </div>
+                    <div class="rating-label2">
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                    </div>
+                  </div>
+                  <div class="star-rating-item" @click="starRating(4)" v-bind:class="{'star-rating-selected': star_num === 4}">
+                    <div class="rating-label1">
+                      基本满意
+                    </div>
+                    <div class="rating-label2">
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                    </div>
+                  </div>
+                  <div class="star-rating-item rating-item-last" @click="starRating(5)" v-bind:class="{'star-rating-selected': star_num === 5}">
+                    <div class="rating-label1">
+                      十分满意
+                    </div>
+                    <div class="rating-label2">
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                      <span class="star-img">
+                        <img src="/static/img/star-off.png">
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="tag-wrap">
+                  <button v-for="tagLable in tagLableArr" :key="tagLable" class="tag-button" @click="tagSelected($lang(tagLable))" v-bind:class="{'tag-button-selected':tagLableSelected===$lang(tagLable)}">{{$lang(tagLable)}}</button>
+                </div>
+                <div class="reject-reason-wrap">
+                  <textarea class="reject-reason-input" v-model="rejectReason" placeholder="请输入驳回理由..."></textarea>
+                </div>
+              </div>
+            </div>
             <div class="set-btn-wrap">
                 <el-button type="sure" @click='toPass'>{{$lang('通过')}}</el-button>
                 <el-button type="danger" @click='isReject=true'>{{$lang('驳回')}}</el-button>
@@ -20,6 +113,90 @@
     </div>
 </template>
 
+<style scoped>
+/* 选择星星个数 */
+.star-rating {
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+}
+.star-rating-item {
+  height: 75px;
+  border-color: #DDD;
+  border-style: solid;
+  border-width: 1px;
+  border-right-width: 0;
+  text-align: center;
+  cursor: pointer;
+  flex: 1;
+}
+.star-rating-selected {
+  background-color: #161f40;
+  color: #FFF;
+}
+.rating-item-last {
+  border-right-width: 1px;
+}
+.star-rating-wrap {
+  text-align: center;
+}
+.rating-wrap {
+    display: inline-block;
+    text-align: center;
+    width: 960px;
+}
+.rating-label1 {
+  margin-top: 17px;
+}
+.rating-label2 {
+  margin-top: 8px;
+}
+.star-img {
+  
+}
+/* 标签 */
+.tag-wrap {
+  height: 75px;
+  font-size: 13px;
+  color: #161f40;
+  display: flex;
+  padding-left: 25px;
+  padding-right: 25px;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+.tag-button {
+  border-color: #161f40;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 3px;
+  margin-right: 5px;
+  padding-left: 6px;
+  padding-right: 6px;
+  color: #161f40;
+  background-color: #fff;
+  height: 25px;
+  cursor: pointer;
+}
+.tag-button-selected {
+  color: #fff;
+  background-color: #161f40;
+}
+/* 驳回理由 */
+.reject-reason-wrap {
+  padding-left: 10px;
+  padding-right: 10px;
+  height: 180px;
+}
+.reject-reason-input {
+  width: calc(100% - 20px);
+  height: calc(100% - 20px);
+  padding: 10px;
+}
+</style>
+
 <script>
 import { AccomplishTask, OverruleTask } from "@/apis/task";
 import { getAllFile } from "@/apis/files";
@@ -28,6 +205,10 @@ import { client } from "@/apis/uploadFile";
 export default {
   data() {
     return {
+      star_num: 5, //星级评价的星星个数
+      tagLableArr: ["预期差异太大", "颜色太亮", "不想要了，设计师没懂我的想法", "画面太满"],
+      tagLableSelected: "", //选中的评价标签
+      rejectReason: "", //驳回理由
       textarea: "",
       isReject: false,
       iframeSrc: ""
@@ -127,6 +308,16 @@ export default {
         iframe.webkitRequestFullScreen ||
         iframe.mozRequestFullScreen
       ).call(iframe);
+    },
+    //评价星星个数
+    starRating(star_num) {
+      const t = this;
+      t.star_num = star_num;
+    },
+    //评价标签
+    tagSelected(tagLableSelected) {
+      const t = this;
+      t.tagLableSelected = tagLableSelected;
     }
   }
 };
